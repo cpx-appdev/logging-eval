67f9928264a696fe7bd50b9e68382e1aa75a4eea
Alexander Tank <Alexander.Tank@comparex.de>
2018-03-01 16:40:59 +0100

    Serilog with seq

--- src/Logging.ConsoleApp/Program.cs
+++ src/Logging.ConsoleApp/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Context;
+using Serilog.Core;
 
 namespace Logging.ConsoleApp
 {
@@ -16,27 +19,29 @@ namespace Logging.ConsoleApp
         async Task Run()
         {
             var sw = Stopwatch.StartNew();
-            using (var logger = new DummyLogger())
+            using (var logger = new SeriLogger())
             {
-                for (int i = 0; i < 50000; i++)
+                for (var i = 0; i < 50000; i++)
                 {
                     logger.SetCorrelationId(Guid.NewGuid());
 
-                    var invoice = new ProcessedInvoice();
-                    invoice.Id = i;
-                    invoice.InvoiceDate = DateTimeOffset.Now;
-                    invoice.Orders = new List<Order>
+                    var invoice = new ProcessedInvoice
                     {
-                        new Order {Id = -i, AuthorizationId = "blub"}
+                        Id = i,
+                        InvoiceDate = DateTimeOffset.Now,
+                        Orders = new List<Order>
+                        {
+                            new Order {Id = -i, AuthorizationId = "blub"}
+                        }
                     };
 
-                    await logger.Info("Erste Meldung", invoice);
-                    await logger.Info("Zweite Meldung", invoice.Orders.ToArray()[0]);
+                    await logger.Info("Erste Meldung {@invoice}", invoice);
+                    await logger.Info("Zweite Meldung {@order}", invoice.Orders.ToArray()[0]);
                 }
-
                 
             }
             Console.WriteLine(sw.Elapsed);
+            Console.ReadKey();
         }
     }
 
@@ -44,5 +49,38 @@ namespace Logging.ConsoleApp
     {
         void SetCorrelationId(Guid id);
         Task Info(string msg, object extraData = null);
+    }
+
+    public class SeriLogger : ILogger
+    {
+        private readonly Logger _logger;
+        private Guid _id;
+
+        public SeriLogger()
+        {
+            _logger = new LoggerConfiguration()
+                .WriteTo.Seq("http://localhost:5341")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+        }
+
+        public void Dispose()
+        {
+            _logger.Dispose();
+        }
+
+        public void SetCorrelationId(Guid id)
+        {
+            _id = id;
+        }
+
+        public Task Info(string msg, object extraData = null)
+        {
+            using (LogContext.PushProperty("correlationId", _id))
+            {
+                _logger.Information(msg, extraData);
+                return Task.CompletedTask;
+            }
+        }
     }
 }
